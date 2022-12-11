db7a109f51d402516e874b8fd6efeda4f9f63c07
Alexey Kuptsov <jerubbaal@mail.ru>
2022-12-11 18:10:04 +0600

    feat: add `cd` command with checker

--- base/command-line.mjs
+++ base/command-line.mjs
@@ -1,17 +1,22 @@
-import { cdUp } from "../fs/chdir.mjs";
+import { cd, cdUp } from "../fs/chdir.mjs";
 import { getStartPath } from "../os/os.mjs";
 import { COL_MAGENTA, COL_RED, COL_RESET } from "./color.mjs";
 
 let currentPath = null
 
-const getResult = (command) => {
+const getResult = async (command) => {
   const argsArr = command.trim().split(' ').filter(param => param);
 
-  if (!argsArr.length) return ''
+  if (!argsArr.length) return '';
   if (argsArr[0] == '.exit' && (argsArr.length == 1)) process.exit(0);
   if (argsArr[0] == 'up' && (argsArr.length == 1)) {
     currentPath = cdUp(currentPath);
-    return ''
+    return '';
+  }
+  if (argsArr[0] == 'cd' && (argsArr.length == 2)) {
+    const [ newPath, message ] = await cd(currentPath, argsArr.slice(1).join(' '));
+    currentPath = newPath;
+    return message;
   }
 
   return `${COL_RED}Invalid input${COL_RESET}\n`;
@@ -19,15 +24,15 @@ const getResult = (command) => {
 
 const showPath = () => {
   if (!currentPath) {
-    currentPath = getStartPath()
+    currentPath = getStartPath();
   }
-  process.stdout.write(`You are currently in ${COL_MAGENTA}${currentPath}${COL_RESET} > `)
+  process.stdout.write(`You are currently in ${COL_MAGENTA}${currentPath}${COL_RESET} > `);
 }
 
 const readCommands = async (chunk) => {
   const command = chunk.toString();
   process.stdout.write(
-    getResult(command)
+    await getResult(command)
   )
   showPath()
 }
--- fs/chdir.mjs
+++ fs/chdir.mjs
@@ -1,3 +1,19 @@
-import path from 'path'
+import path, { resolve } from 'path'
+import fs from 'fs'
+import { COL_RED, COL_RESET } from '../base/color.mjs';
+import { rejects } from 'assert';
 
 export const cdUp = (currentPath) => path.parse(currentPath).dir;
+
+export const cd = async (currentPath, toPath) => {
+  toPath.replaceAll('"', '');
+  const newPath = path.join(currentPath, toPath);
+  return await new Promise((resolve) => { 
+    fs.stat(newPath, (err, stat) => {
+      if (err || !stat.isDirectory()) {
+        resolve([ currentPath, `${COL_RED}wrong path!${COL_RESET}\n` ]) 
+      }
+      resolve([ newPath, '' ])
+    });
+  });
+}
